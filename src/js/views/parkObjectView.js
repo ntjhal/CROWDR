de39ada605d2cfd0d0295b81c4fa6cb371960f61
Niels van Hal <ntj.vanhal@student.avans.nl>
2021-03-29 12:20:05 +0200

    object update method

--- src/js/views/parkObjectView.js
+++ src/js/views/parkObjectView.js
@@ -12,7 +12,18 @@ export class ParkObjectDetailView {
         `<p><strong>Type:</strong> ${object.type[0].toUpperCase() + object.type.substring(1)}</p>` +
         `<p><strong>Width:</strong> ${object.width}</p>` +
         `<p><strong>Height:</strong> ${object.height}</p>`;
+   
+    }
 
-        
+    updateObject(regionID, object) {
+        let region = this.regioncontroller.getRegion(regionID);
+
+        let updatedObjects = region.parkObjects;
+        updatedObjects.filter(o => o.id !== object.id);
+        updatedObjects.push(object);
+
+        region.parkObjects = updatedObjects;
+
+        this.regioncontroller.saveRegion(region);
     }
 }
