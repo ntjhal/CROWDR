8a24d30cfb01e0cdd7768a200cec5885e517d3f7
matsn0w <bartscholtus@gmail.com>
2021-04-01 18:58:30 +0200

    handle weather api errors, fixed icons

--- src/js/models/weatherModel.js
+++ src/js/models/weatherModel.js
@@ -2,7 +2,19 @@ const API_KEY = 'ae49cf85a4eba3f2c59b1f0ab5e1a222'; // this is unsafe
 
 export class WeatherModel {
     async getWeather(city) {
-        return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric&lang=nl`)
-            .then(res => res.json())
+        return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric&lang=en`)
+            .then(res => {
+                if (!res.ok) {
+                    throw 'API connection failed!';
+                }
+                
+                return res;
+            })
+            .then(res => {
+                return res.json();
+            })
+            .catch(e => {
+                throw e;
+            });
     }
 }
