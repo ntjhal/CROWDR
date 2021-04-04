b80d07ffa436eb0e66a717446d63ab26ea405016
matsn0w <bartscholtus@gmail.com>
2021-04-04 15:40:15 +0200

    oops

--- src/js/models/fetcher.js
+++ src/js/models/fetcher.js
@@ -1,22 +1,9 @@
 export class Fetcher {
     static async get(url) {
-        return fetch(url, {
-            method: 'GET',
-            mode: 'no-cors',
-            headers: {    
-                'Access-Control-Allow-Origin': '*' 
-            },
-        })
-            .then(res => {
-                if (!res.ok) {
-                    throw 'API connection failed!';
-                }
-                
-                return res;
-            })
+        return fetch(url)
             .then(res => res.json())
             .catch(e => {
-                throw e;
+                throw 'API connection failed!';
             });
     }
 }
